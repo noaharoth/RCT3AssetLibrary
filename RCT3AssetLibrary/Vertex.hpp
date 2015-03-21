6215782049b02057374f42228a7b919674e97bc3
Noah Roth <noaharoth.dev@gmail.com>
2015-03-21 17:08:02 -0600

    Relocated BGRAColor struct to Vertex header

--- RCT3AssetLibrary/Vertex.hpp
+++ RCT3AssetLibrary/Vertex.hpp
@@ -16,9 +16,21 @@ namespace RCT3Asset
 
 	struct GameColor
 	{
-		unsigned int Color1, Color2, Color3;
+		unsigned int Color1;
+		unsigned int Color2;
+		unsigned int Color3;;
 
 		GameColor() : Color1(0), Color2(0), Color3(0) { }
+	};
+
+	struct BGRAColor
+	{
+		unsigned char B;
+		unsigned char G;
+		unsigned char R;
+		unsigned char A;
+
+		BGRAColor() : B(0), G(0), R(0), A(0) { }
 	};
 
 }
